7cd76d542dc905bc3cb9070aa74f9880dc5300eb
AreyBRSK <areybrsk@gmail.com>
2022-06-10 15:49:11 +0300

    Model setup

---
+++ HW3.3/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  HW3.3
+//
+//  Created by Андрей Барсук on 10.06.2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    var name = ""
+    var isRegistered = false
+}
